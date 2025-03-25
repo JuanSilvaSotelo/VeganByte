834faa6e553772d86c8b001c7c1ec14e6d611862
JuanSilva <juan_dsilvas@soy.sena.edu.co>
2025-03-24 22:56:35 -0500

    Trabajando en la pagina de inicio y optimizacion de CSS

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/react.svg';
+import image4 from "../assets/images/image-4.png";
 
 function Header() {
     return (
         <header>
             <div className="logo-container">
                 <img src={logo} alt="Logo" />
-                <span>madre * raíz</span>
+                <span> Casa taller Madre Raíz</span>
             </div>
+            <nav>
+                <Link to="/login" className="mx-2">Iniciar Sesión</Link>
+                <Link to="/register" className="mx-2">Registrarse</Link>
+            </nav>
             <nav>
                 <Link to="/" className="mx-2">Inicio</Link>
                 <a href="#" className="mx-2">Quienes Somos</a>
                 <a href="#" className="mx-2">Galería</a>
                 <a href="#" className="mx-2">Blog</a>
                 <a href="#" className="mx-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 240 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                    </svg>
+                    <img className="calendar" alt="calendar" src={image4} />
                 </a>
-                <Link to="/login" className="mx-2">Iniciar Sesión</Link>
-                <Link to="/register" className="mx-2">Registrarse</Link>
             </nav>
         </header>
     );
