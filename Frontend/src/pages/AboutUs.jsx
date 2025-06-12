8ab32d68b982259ba458bc5aff95dbe806b7e54d
JuanSilva <juan_dsilvas@soy.sena.edu.co>
2025-06-11 20:32:43 -0500

    Archivos

---
+++ Frontend/src/pages/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>Quiénes Somos</h1>
+      <p>Esta es la página de Quiénes Somos.</p>
+    </div>
+  );
+};
+
+export default AboutUs;
