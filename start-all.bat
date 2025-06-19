cb2d4ba44b1aeec82958745fa89627d020feb40e
Juan Silva <148798352+JuanSilvaSotelo@users.noreply.github.com>
2025-06-19 17:55:59 -0500

    Update start-all.bat

--- start-all.bat
+++ start-all.bat
@@ -3,10 +3,23 @@ ECHO Iniciando Backend...
 cd Backend
 ECHO Creando y configurando la base de datos...
 cmd /c ""C:\Program Files\MySQL\MySQL Server 9.2\bin\mysql.exe" -u root -p1604 < ""%~dp0DB\DB.sql""" 2>NUL
-start npm run dev
+start npm start
+start http://localhost:5173/
 
 ECHO Iniciando Frontend...
 cd ..\Frontend
-npm run dev
+start npm run dev
 
 ECHO Ambos servidores se estan iniciando en nuevas ventanas.
+
+:WAIT_FOR_SHUTDOWN
+SET /P "input=Escribe 'cerrar' para apagar los servidores: "
+IF /I "%input%"=="cerrar" GOTO SHUTDOWN
+GOTO WAIT_FOR_SHUTDOWN
+
+:SHUTDOWN
+ECHO Apagando servidores...
+REM Intenta terminar todos los procesos de Node.js. Esto podría afectar otras aplicaciones de Node.js en ejecución.
+taskkill /IM node.exe /F /T >NUL 2>&1
+ECHO Servidores apagados.
+EXIT
